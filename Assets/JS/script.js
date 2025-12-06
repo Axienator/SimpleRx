1ecee7f8189060ee3af69d0310358de0f058125f
Axienator <205501519+Axienator@users.noreply.github.com>
2025-12-06 13:38:24 +0800

    Add contents, Small changes

---
+++ Assets/JS/script.js
@@ -0,0 +1,20 @@
+const Symptom = {
+    sympOne: document.querySelector('.symCheckBox1'),
+    sympTwo: document.querySelector('.symCheckBox2'),
+    sympThree: document.querySelector('.symCheckBox3'),
+    sympFour: document.querySelector('.symCheckBox4'),
+    sympFive: document.querySelector('.symCheckBox5'),
+    sympSix: document.querySelector('.symCheckBox6'),
+    sympSeven: document.querySelector('.symCheckBox7'),
+    sympEight: document.querySelector('.symCheckBox8'),
+}
+
+// Group the Symptom into three, Mild, Moderate, Severe
+
+// if the user Includes a Symptom that is Mild = Result will appear as Mild
+
+// If one or more severe symptom were checked it will appear as Urgent Attention
+
+// If one or more moderate symptom were checked it will appear as Medical Consultation
+
+// If one or more mild symptom were checked it will appear as Monitor your Symptom
